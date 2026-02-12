952fb8a46e66e0d9cb4dc8595842b477657d78fa
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2026-02-12 17:47:16 +0100

    added checksum.py

---
+++ romcheck/setup.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+REM setup.bat template
+
+REM Set the PYTHONDIR to path for python.exe (must end in \)
+SET PYTHONDIR=C:\Programs\Python\
+IF NOT EXIST %PYTHONDIR%python.exe (
+  ECHO No python.exe in %PYTHONDIR%
+  ECHO Patch line 5 in %~f0
+  EXIT /b
+)
+
+ECHO Creating virtual Python environment
+%PYTHONDIR%python.exe -m venv env
+CALL env\Scripts\activate.bat
+ECHO Upgrading pip
+python -m pip install -q --upgrade pip setuptools wheel
+ECHO Adding packages
+IF EXIST requirements.txt (
+   pip install -q -r requirements.txt
+)
+
+ECHO.Setup done, now use run
+
